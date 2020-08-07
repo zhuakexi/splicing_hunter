1d39410f8b43c61c07754f36c0296f34c8dcfc88
zhuakexi <zhuakexi@126.com>
2020-08-07 17:33:59 +0800

    new entrance for block_search, using index to speed up

--- hunt.py
+++ hunt.py
@@ -1,22 +1,19 @@
 import sys
 import pickle
-ref_name = sys.argv[1]
-bin_name = sys.argv[2]
-cell_name = sys.argv[3]
-from parser import bed_parser, bin_parser, con_parser
-from block_search import build_index, block_search
-import random
-chromsomes, ref_df = bed_parser(ref_name,"on")
-cell = con_parser(cell_name)
-bins = bin_parser(bin_name,"on")
-''' build index using 1/x fold sampling for test
-sample1 = {name:random.sample(chromsomes[name],len(chromsomes[name])//10) for name in chromsomes}
-bin_index = build_index(bins, 10000, sample1)
-with open("ref/bin_10k_d10_index","wb") as f:
-    pickle.dump(bin_index,f)
-'''
-# load directly from pickled bin_index
-with open("ref/bin_10k_d10_index","rb") as f:
-    bin_index = pickle.load(f)
 
-result = block_search(bin_index, 10000, cell)
+from parser import con_parser
+from block_search import block_search
+import random
+
+BINSIZE = 10000
+
+index_name = sys.argv[1]
+cell_name = sys.argv[2]
+cell = con_parser(cell_name)
+
+# load directly from pickled bin_index
+with open(index_name,"rb") as f:
+    bin_index = pickle.load(f)
+result = block_search(bin_index, BINSIZE, cell)
+print("\n".join([str(i) for i in result]))
+sys.stderr.write("block_search total questionable contacts: " + str(len(result)) + "\n")
